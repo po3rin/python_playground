ea182f2b3b12a495051a7daa0f6fe1fedec214ca
中村 弘武 <abctail30@gmail.com>
2020-11-16 20:32:58 +0900

    pool

--- pool-share-memory/main.py
+++ pool-share-memory/main.py
@@ -1,14 +1,25 @@
 from multiprocessing import Pool
+from typing import Dict
 import time
 
 
 def sleep(name):
-    time.sleep(3)
+    print(global_cache)
+    if name not in global_cache:
+        print("not in")
+        global_cache[name] = name
+        time.sleep(3)
     print(name)
     return name
 
 
-list = [1, 2, 3, 4, 5]
-with Pool(processes=3) as p:
+def init_global_cache(cache: Dict[str, str]) -> None:
+    global global_cache
+    global_cache = cache
+
+
+cache = {}
+list = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+with Pool(processes=3, initializer=init_global_cache, initargs=(cache,)) as p:
     r = p.map(func=sleep, iterable=list)
     print(r)
