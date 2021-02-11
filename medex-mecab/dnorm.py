2c19f12bf6e58dc922b936554730537ee6c9a273
中村 弘武 <abctail30@gmail.com>
2021-02-12 01:53:50 +0900

    change alias

--- medex-mecab/dnorm.py
+++ medex-mecab/dnorm.py
@@ -1,5 +1,13 @@
 from dnorm_j import DNorm
 
 model = DNorm.from_pretrained()
-result = model.normalize('胃がん')
+result = model.normalize('水いぼ')
+print(result)
+result = model.normalize('胃ガン')
+print(result)
+result = model.normalize('乳がん')
+print(result)
+result = model.normalize('子宮頸管ポリープ')
+print(result)
+result = model.normalize('腟カンジダ症')
 print(result)
