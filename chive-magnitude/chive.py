00c01e97beb16cdc7eac0abd3b6e426c8d8fe6f3
中村 弘武 <abctail30@gmail.com>
2021-03-28 19:16:25 +0900

    rename key

--- chive-magnitude/chive.py
+++ chive-magnitude/chive.py
@@ -13,13 +13,7 @@ vectors = Magnitude(
 # ローカルにファイルをダウンロードせず、ベクトルをすばやく取得
 # vectors = Magnitude("https://sudachi.s3-ap-northeast-1.amazonaws.com/chive/chive-1.1-mc90-aunit.magnitude", stream=True)
 
-print(vectors.similarity("はり", "張り"))
+print(vectors.similarity("痛み", "病気"))
 print(vectors.similarity("痛み", "痛い"))
-print(vectors.similarity("腹", "お腹"))
-print(vectors.similarity("腹", "腹"))
 print(vectors.similarity("妊娠中", "妊娠"))
-print(vectors.similarity("妊娠初期", "妊娠"))
-print(vectors.similarity("妊娠", "痛み"))
 print(vectors.similarity("ぶつけた", "ぶつける"))
-print(vectors.similarity("頭", "こぶ"))
-print(vectors.similarity("打った", "ぶつけた"))
