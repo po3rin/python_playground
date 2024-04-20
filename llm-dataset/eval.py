a2b199258c4d10fb8ab2f69da6e4515d9bb95bcb
hiromu nakamura <hiromunakamura@hiromus-MacBook-Air.local>
2024-04-21 02:08:35 +0900

    update

--- llm-dataset/eval.py
+++ llm-dataset/eval.py
@@ -2,11 +2,12 @@ import random
 from statistics import mean
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 from elasticsearch import Elasticsearch
 
 
 def dcg(gain, k=None):
-    """ calc dcg value """ 
+    """calc dcg value"""
     if k is None:
         k = gain.shape[0]
 
@@ -17,116 +18,91 @@ def dcg(gain, k=None):
 
 
 def ndcg(y, k=None, powered=False) -> float:
-    """ calc nDCG value """
+    """calc nDCG value"""
 
     dcg_score = dcg(y, k=k)
 
     ideal_sorted_scores = np.sort(y)[::-1]
     ideal_dcg_score = dcg(ideal_sorted_scores, k=k)
-    
+
     return dcg_score / ideal_dcg_score
 
 
 def main():
-  client = Elasticsearch(hosts=['http://localhost:9200'])
-  if not client.ping():
-    raise Exception('Elasticsearch is not running.')
+    client = Elasticsearch(hosts=["http://localhost:9200"])
+    if not client.ping():
+        raise Exception("Elasticsearch is not running.")
 
-  df = pd.read_csv('data/dataset.csv')
-  docs = df.to_dict(orient='records')
-  queries = df['query'].drop_duplicates().tolist()
-  random.shuffle(docs)
+    df = pd.read_csv("data/dataset.csv")
+    docs = df.to_dict(orient="records")
+    queries = df["query"].drop_duplicates().tolist()
+    random.shuffle(docs)
 
-  # for d in docs:
-  #   client.index(index='docs', body=d, pipeline='japanese-text-embeddings')
+    for d in docs:
+      client.index(index='docs', body=d, pipeline='japanese-text-embeddings')
 
-  results_a = []
-  results_b = []
-  for q in queries:
-    result_a = client.search(index='docs', body={
-      "_source": {
-        "includes": ["title", "body", "score", "reason"]
-      },
-      "query": {
-          "bool": {
-              "should": {
-                  "match": {
-                      "title": q
-                  }
-              },
-              "filter": {
-                  "match": {
-                      "query": q
-                  }
-              }
-          }
-      }
-    })
-    scores_a = [r['_source']['score'] for r in result_a['hits']['hits']]
+    results_a = []
+    results_b = []
+    for q in tqdm(queries):
+        result_a = client.search(
+            index="docs",
+            body={
+                "_source": {"includes": ["title", "body", "score", "reason"]},
+                "query": {
+                    "bool": {
+                        "should": {"match": {"title": q}},
+                        "filter": {"match": {"query": q}},
+                    }
+                },
+            },
+        )
+        scores_a = [r["_source"]["score"] for r in result_a["hits"]["hits"]]
 
-    result_b = client.search(index='docs', body={
-      "_source": {
-        "includes": ["title", "body", "score", "reason"]
-      },
-      "query": {
-          "bool": {
-              "should": {
-                  "match": {
-                      "title": q
-                  }
-              },
-              "filter": {
-                  "match": {
-                      "query": q
-                  }
-              }
-          }
-      },
-      "knn": {
-          "boost": 2,
-          "field": "text_embedding.predicted_value",
-          "k": 10,
-          "num_candidates": 50,
-          "query_vector_builder": {
-              "text_embedding": {
-                  "model_id": "cl-tohoku__bert-base-japanese-v2",
-                  "model_text": q
-              }
-          },
-          "filter": {
-              "bool": {
-                  "filter": {
-                      "match": {
-                          "query": q
-                      }
-                  }
-              }
-          }
-      }
-    })
+        result_b = client.search(
+            index="docs",
+            body={
+                "_source": {"includes": ["title", "body", "score", "reason"]},
+                "query": {
+                    "bool": {
+                        "should": {"match": {"title": q}},
+                        "filter": {"match": {"query": q}},
+                    }
+                },
+                "knn": {
+                    "field": "text_embedding.predicted_value",
+                    "k": 10,
+                    "num_candidates": 50,
+                    "query_vector_builder": {
+                        "text_embedding": {
+                            "model_id": "cl-tohoku__bert-base-japanese-v2",
+                            "model_text": q,
+                        }
+                    },
+                    "filter": {"bool": {"filter": {"match": {"query": q}}}},
+                },
+            },
+        )
 
-    
-    scores_a = [r['_source']['score'] for r in result_a['hits']['hits']]
-    scores_b = [r['_source']['score'] for r in result_b['hits']['hits']]
-    print('===========================')
-    print(f'query: {q}')
-    print('---------scores------------')
-    print(scores_a)
-    print(scores_b)
-    print('---------------------------')
-    if len(scores_a) == 0 or len(scores_b) == 0:
-      continue
+        scores_a = [r["_source"]["score"] for r in result_a["hits"]["hits"]]
+        scores_b = [r["_source"]["score"] for r in result_b["hits"]["hits"]]
+        # print("===========================")
+        # print(f"query: {q}")
+        # print("---------scores------------")
+        # print(scores_a)
+        # print(scores_b)
+        # print("---------------------------")
+        if len(scores_a) == 0 or len(scores_b) == 0:
+            continue
 
-    results_a.append(ndcg(np.array(scores_a)))
-    results_b.append(ndcg(np.array(scores_b)))
+        results_a.append(ndcg(np.array(scores_a)))
+        results_b.append(ndcg(np.array(scores_b)))
 
-  print(results_a)
-  print(results_b)
+    # print(results_a)
+    # print(results_b)
 
-  print(mean(results_a))
-  print(mean(results_b))
+    print(mean(results_a))
+    print(mean(results_b))
 
 
-
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
